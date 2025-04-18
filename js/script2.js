437d257e11466f2dd7a2926860ea8ad22b9bdbbf
EdoardoPosfortunato <edoardo.posfortunato27@gmail.com>
2025-04-18 14:14:28 +0200

    feat : includes

--- js/script2.js
+++ js/script2.js
@@ -1,20 +1,32 @@
-const world = "forchettauuEEE"
+const world = "aIUoLa"
 console.warn(`Quante vocali contiene la parola ${world}?`);
 
 console.log(world)
 
-const vocali = ["a", "e", "i", "o", "u"]
+const vocali = ["A", "E", "I", "O", "U"]
 
 // console.log(vocali[1].toUpperCase())
 
 let contatore = 0;
 
-for (let n = 0; n < vocali.length; n++) {
-    for (let i = 0; i < world.length; i++) {
-        if (world[i] === vocali[n] || world[i] === vocali[n].toUpperCase()) {
-            contatore = contatore + 1;
-        }
-    }
-}
+//CON 2 CICLIFOR
 
-console.log(`La parola ${world} è composta da ${contatore} vocali`)
+// for (let n = 0; n < vocali.length; n++) {
+//     for (let i = 0; i < world.length; i++) {
+//         if (world[i] === vocali[n] || world[i] === vocali[n].toUpperCase()) {
+//             contatore = contatore + 1;
+//         }
+//     }
+// }
+
+// console.log(`La parola ${world} è composta da ${contatore} vocali`)
+
+for (let n = 0; n < world.length; n++) {
+
+            if (vocali.includes(world[n].toUpperCase())) {
+                contatore = contatore + 1;
+            }
+
+    }
+    
+    console.log(`La parola ${world} è composta da ${contatore} vocali`)
